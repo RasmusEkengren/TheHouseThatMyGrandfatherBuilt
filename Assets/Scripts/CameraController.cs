c1469573ddba596d6d45ec1c728da5794fa1b499
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2021-04-06 13:05:30 +0200

    Added camera smoothing and a test scene

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,15 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
 	public GameObject player;
-	public Vector3 cameraOffset;
 
-    // Update is called once per frame
-    void Update()
-    {
-		transform.position = player.transform.position + cameraOffset;
-    }
+	public Vector3 cameraOffset;
+	public float smoothTime;
+	public float smoothLength;
+
+	private Vector3 velocity = Vector3.zero;
+    void LateUpdate()
+	{
+		Vector3 targetPosition = player.transform.position + cameraOffset;
+		if (Vector3.Distance(transform.position, targetPosition) > smoothLength)
+		{
+			Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+			transform.position = smoothPosition;
+		}
+	}
 }
