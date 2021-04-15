9dfdf5d2540dc2ef4b49939df5c9529dbc73b15a
Jens Bakklund <je2ba@outlook.com>
2021-04-15 11:05:56 +0200

    ButtonSound script

--- Assets/Scripts/ButtonSound.cs
+++ Assets/Scripts/ButtonSound.cs
@@ -6,39 +6,21 @@ using UnityEngine.EventSystems;
 
 public class ButtonSound : MonoBehaviour
 {
-    private Button button = null;
-
-    [Space]
     [FMODUnity.EventRef] public string hoverSound;
     [FMODUnity.EventRef] public string clickSound;
 
-    private void Start()
-    {
-        button = GetComponent<Button>();
-    }
-    
-    public void OnPointerEnter(PointerEventData eventData)
+    public void PlayHoverSound()
     {
         if (hoverSound.Length > 0)
         {
             FMODUnity.RuntimeManager.PlayOneShot(hoverSound);
-        }    
+        }
     }
-
-    public void OnMouseEnter()
-    {
-    }
-
-
-    public void OnMouseDown()
-    {
-    }
-
-    public void OnPoinerClick(PointerEventData eventData)
+    public void PlayClickSound()
     {
         if (clickSound.Length > 0)
         {
             FMODUnity.RuntimeManager.PlayOneShot(clickSound);
-        }       
+        }
     }
 }
