d99d1501f17e844ee2f341d3387f2290de7f6a3f
Jens Bakklund <je2ba@outlook.com>
2021-04-21 11:21:08 +0200

    Started on Serialization script

---
+++ Assets/Scripts/SceneManagment/SceneLevel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Animations;
+using UnityEngine.Events;
+using FMODUnity;
+
+namespace Scene
+{
+	public class SceneLevel : MonoBehaviour
+	{
+        public int sceneLevel;
+        public enum SceneType { Leah, George };
+        public SceneType sceneType;
+
+        private void Update()
+        {
+            
+        }
+    }
+}
