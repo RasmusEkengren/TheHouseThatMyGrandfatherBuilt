a2eff67bcbaf0eb17eaf8394a36619b429754778
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2021-04-26 16:07:55 +0200

    Fixing some UI and scripts tied to them

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 
 public class CutsceneManager : MonoBehaviour
 {
+	[SerializeField] private float borderSpeed;
 	[SerializeField] private RectTransform topBorder;
 	[SerializeField] private RectTransform bottomBorder;
 	[SerializeField] private UnityEvent startEvents;
@@ -14,12 +15,25 @@ public class CutsceneManager : MonoBehaviour
 	}
 	public void StartBorder()
 	{
-		topBorder.gameObject.SetActive(true);
-		bottomBorder.gameObject.SetActive(true);
+		StartCoroutine(MoveBorder(true));
 	}
 	public void EndBorder()
 	{
-		topBorder.gameObject.SetActive(false);
-		bottomBorder.gameObject.SetActive(false);
+		StartCoroutine(MoveBorder(false));
+	}
+	private IEnumerator MoveBorder(bool isGoingIn)
+	{
+		float topTargetValue = isGoingIn ? 0.87037f : 1;
+		float botTargetValue = isGoingIn ? 0.12963f : 0;
+		float topStart = topBorder.anchorMin.y;
+		float botStart = bottomBorder.anchorMax.y;
+		float t = 0;
+		while (t < 1)
+		{
+			topBorder.anchorMin = new Vector2(0, Mathf.Lerp(topStart, topTargetValue, t));
+			bottomBorder.anchorMax = new Vector2(1, Mathf.Lerp(botStart, botTargetValue, t));
+			t += Time.deltaTime * borderSpeed;
+			yield return null;
+		}
 	}
 }
--- Assets/Scripts/UI/ChoiceButton.cs
+++ Assets/Scripts/UI/ChoiceButton.cs
@@ -7,27 +7,56 @@ using UnityEngine.EventSystems;
 
 public class ChoiceButton : MonoBehaviour, IPointerEnterHandler, ISelectHandler, IDeselectHandler
 {
+	[SerializeField] private float TimeToSelectable = 0.2f;
+	[SerializeField] private bool isSelectedAtStart = false;
 	[SerializeField] private Animator animator = null;
 	[SerializeField] private EventSystem eventSystem;
 	private Button button;
+	private bool isSelectable;
+	private float timer = 0;
 	private string isSelected = "IsSelected";
 
 	void OnEnable()
 	{
 		button = GetComponent<Button>();
+		timer = 0;
+		isSelectable = false;
+	}
+	void Update()
+	{
+		if (!isSelectable)
+		{
+			if (timer < TimeToSelectable)
+			{
+				timer += Time.deltaTime;
+			}
+			else if (isSelectedAtStart)
+			{
+				isSelectable = true;
+				eventSystem.SetSelectedGameObject(null);
+				button.Select();
+			}
+			else
+			{
+				isSelectable = true;
+			}
+		}
 	}
 	public void OnPointerEnter(PointerEventData pointerData)
 	{
-		eventSystem.SetSelectedGameObject(null);
-		button.Select();
-		button.OnSelect(null);
+		if (isSelectable)
+		{
+			eventSystem.SetSelectedGameObject(null);
+			button.Select();
+			button.OnSelect(null);
+		}
 	}
 	public void OnSelect(BaseEventData selectData)
 	{
-		if (selectData.selectedObject == this.gameObject) animator.SetBool(isSelected, true);
+		if (selectData.selectedObject == this.gameObject && isSelectable) animator.SetBool(isSelected, true);
 	}
 	public void OnDeselect(BaseEventData deselectData)
 	{
-		if (deselectData.selectedObject == this.gameObject) animator.SetBool(isSelected, false);
+		if (deselectData.selectedObject == this.gameObject && isSelectable) animator.SetBool(isSelected, false);
 	}
 }
