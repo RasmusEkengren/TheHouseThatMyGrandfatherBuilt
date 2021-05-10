e5046e687d781d1ed724a476bd56fd83a6ffd42b
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2021-05-10 15:35:53 +0200

    Saving stuff inbetween scenes is now working! YAY

---
+++ Assets/Scripts/ObjectState.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ObjectState : MonoBehaviour
+{
+	[Serializable]
+	private class BoolStates
+	{
+		public enum activeStates { ON, OFF, NONE }
+		public activeStates OnPorchFixed = activeStates.NONE;
+		public activeStates OnPorchFlat = activeStates.NONE;
+		public activeStates OnPorchSlanted = activeStates.NONE;
+		public activeStates OnPorchBroken = activeStates.NONE;
+		public activeStates OnWindowsFixed = activeStates.NONE;
+		public activeStates OnWindowsBroken = activeStates.NONE;
+		public activeStates OnGeorgeStatePorch = activeStates.NONE;
+		public activeStates OnGeorgeStateWindows = activeStates.NONE;
+	}
+	[SerializeField] private BoolStates boolStates;
+	public void SetState()
+	{
+		if (GlobalSceneData.mg_porchFixed)
+		{
+			switch (boolStates.OnPorchFixed)
+			{
+				case BoolStates.activeStates.ON:
+					this.gameObject.SetActive(true);
+					break;
+				case BoolStates.activeStates.OFF:
+					this.gameObject.SetActive(false);
+					return;
+				default:
+					break;
+			}
+			if (GlobalSceneData.porchFixed == GlobalSceneData.PorchFixed.Flat)
+			{
+				switch (boolStates.OnPorchFlat)
+				{
+					case BoolStates.activeStates.ON:
+						this.gameObject.SetActive(true);
+						break;
+					case BoolStates.activeStates.OFF:
+						this.gameObject.SetActive(false);
+						return;
+					default:
+						break;
+				}
+			}
+			else if (GlobalSceneData.porchFixed == GlobalSceneData.PorchFixed.Slanted)
+			{
+				switch (boolStates.OnPorchSlanted)
+				{
+					case BoolStates.activeStates.ON:
+						this.gameObject.SetActive(true);
+						break;
+					case BoolStates.activeStates.OFF:
+						this.gameObject.SetActive(false);
+						return;
+					default:
+						break;
+				}
+			}
+		}
+		else
+		{
+			switch (boolStates.OnPorchBroken)
+			{
+				case BoolStates.activeStates.ON:
+					this.gameObject.SetActive(true);
+					break;
+				case BoolStates.activeStates.OFF:
+					this.gameObject.SetActive(false);
+					return;
+				default:
+					break;
+			}
+		}
+		if (GlobalSceneData.mg_windowsFixed)
+		{
+			switch (boolStates.OnWindowsFixed)
+			{
+				case BoolStates.activeStates.ON:
+					this.gameObject.SetActive(true);
+					break;
+				case BoolStates.activeStates.OFF:
+					this.gameObject.SetActive(false);
+					return;
+				default:
+					break;
+			}
+		}
+		else
+		{
+			switch (boolStates.OnWindowsBroken)
+			{
+				case BoolStates.activeStates.ON:
+					this.gameObject.SetActive(true);
+					break;
+				case BoolStates.activeStates.OFF:
+					this.gameObject.SetActive(false);
+					return;
+				default:
+					break;
+			}
+		}
+		if (GlobalSceneData.georgeState == GlobalSceneData.GeorgeState.Porch)
+		{
+			switch (boolStates.OnGeorgeStatePorch)
+			{
+				case BoolStates.activeStates.ON:
+					this.gameObject.SetActive(true);
+					break;
+				case BoolStates.activeStates.OFF:
+					this.gameObject.SetActive(false);
+					return;
+				default:
+					break;
+			}
+		}
+		else if (GlobalSceneData.georgeState == GlobalSceneData.GeorgeState.Windows)
+		{
+			switch (boolStates.OnGeorgeStateWindows)
+			{
+				case BoolStates.activeStates.ON:
+					this.gameObject.SetActive(true);
+					break;
+				case BoolStates.activeStates.OFF:
+					this.gameObject.SetActive(false);
+					return;
+				default:
+					break;
+			}
+		}
+	}
+}
