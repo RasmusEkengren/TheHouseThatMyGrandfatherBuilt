e5d7333abe0ec4eea5eec3d9bfd5edc50addad60
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2021-05-21 13:36:03 +0200

    New Leah Intro

--- Assets/Scripts/CollisionTrigger.cs
+++ Assets/Scripts/CollisionTrigger.cs
@@ -5,11 +5,16 @@ using UnityEngine.Events;
 
 public class CollisionTrigger : MonoBehaviour
 {
-    public UnityEvent eventsToTrigger = null;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Collision trigger", gameObject);
-        eventsToTrigger.Invoke();
-    }
+	[SerializeField] private bool isOneTime = false;
+	public UnityEvent eventsToTrigger = null;
+	private bool isTriggered = false;
+	private void OnTriggerEnter(Collider other)
+	{
+		if (!isTriggered)
+		{
+			isTriggered = isOneTime;
+			Debug.Log("Collision trigger", gameObject);
+			eventsToTrigger.Invoke();
+		}
+	}
 }
