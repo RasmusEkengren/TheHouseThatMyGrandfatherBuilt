4caaa7ce40c1133489a87556185227016566d3ae
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2021-05-24 16:53:00 +0200

    Sawing bug fix, Interactable icon reset, + george window shutters

--- Assets/Scripts/EventOnStart.cs
+++ Assets/Scripts/EventOnStart.cs
@@ -6,8 +6,25 @@ using UnityEngine.Events;
 public class EventOnStart : MonoBehaviour
 {
 	[SerializeField] private UnityEvent startEvents;
+	[SerializeField] private bool isTriggeredOnce = false;
 	void Start()
 	{
-		startEvents.Invoke();
+		if (isTriggeredOnce)
+		{
+			this.gameObject.GetComponent<UniqueID>().CheckID();
+			if (GlobalSceneData.FindInteractedState(this.gameObject.GetComponent<UniqueID>().ID))
+			{
+				return;
+			}
+			else
+			{
+				startEvents.Invoke();
+				GlobalSceneData.interactedObjectIDs.Add(this.gameObject.GetComponent<UniqueID>().ID);
+			}
+		}
+		else
+		{
+			startEvents.Invoke();
+		}
 	}
 }
