e83964194e8a7829ff344174ce31254e5fe3e436
Rasmus Ekengren <ekengrenrasmus@gmail.com>
2021-06-06 14:15:57 +0200

    Final? bug fixes

---
+++ Assets/debugEscape.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class debugEscape : MonoBehaviour
+{
+	public void PressButton()
+	{
+		Debug.Log("ButtonPressed");
+	}
+}
